d930a4df20da239d888c0cab0a7259340c429e42
ChaturGopalkrishna <48550856+ChaturGopalkrishna@users.noreply.github.com>
2026-06-22 15:50:37 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'sc-tracker-v1';
+const ASSETS = [
+  '/sc-tracker/',
+  '/sc-tracker/index.html',
+  '/sc-tracker/manifest.json',
+  '/sc-tracker/icon-192.png',
+  '/sc-tracker/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
